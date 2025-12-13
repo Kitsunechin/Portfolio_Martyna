73393567e1f86d994149f17f39d258efc375765e
Kitsunechin <martyna.b@hotmail.com>
2025-12-13 19:03:18 +0100

    update footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,5 +1,5 @@
 
-import { Mail, Linkedin, ArrowUp } from "lucide-react";
+import { Mail, Linkedin, Github, ArrowUp } from "lucide-react";
 import { Link } from "react-router-dom";
 
 const Footer = () => {
